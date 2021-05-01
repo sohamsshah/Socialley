d78fc95f39ff29933e805ec819c66de1ebcd623a
prerana1821 <prerananawar1@gmail.com>
2021-05-01 15:24:14 +0530

    feat: added dotenv

--- client/src/auth/auth0Provider.jsx
+++ client/src/auth/auth0Provider.jsx
@@ -4,8 +4,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
   const navigate = useNavigate();
-  const domain = process.env.AUTH0_DOMAIN;
-  const clientId = process.env.AUTH0_CLIENT_ID;
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  console.log(domain);
+  console.log(clientId);
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
