3493bc97e151eaa41003eceb4e48df9678645fc3
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-05-01 15:15:21 +0530

    added router

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Auth0ProviderWithHistory from "./auth/auth0Provider";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0ProviderWithHistory>
-      <App />
-    </Auth0ProviderWithHistory>
+    <Router>
+      <Auth0ProviderWithHistory>
+        <App />
+      </Auth0ProviderWithHistory>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
