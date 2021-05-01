d246caab136948e7f5724b2c38864d110f3ffd4a
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-05-01 11:30:58 +0530

    added auth provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import { LandingPage } from "./pages/LandingPage/";
+import Auth0ProviderWithHistory from "./auth/auth0Provider";
 
 function App() {
   return (
     <div className="App">
-      Hello World! I am Socialley.
+      {/* <h1 className="text-red-500">Hello World! I am Socialley.</h1> */}
+      <Auth0ProviderWithHistory>
+        <LandingPage />
+      </Auth0ProviderWithHistory>
     </div>
   );
 }
