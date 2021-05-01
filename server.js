24e469a69932c261320e02097d8faa12414383e8
sohamsshah <sohamshah456@gmail.com>
2021-05-01 21:12:52 +0530

    fix: build static website route

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get('*', (req, res) => res.sendFile(path.resolve('client/build/public', 'index.html')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 }
 
 const user = require("./server/routes/user.router");
