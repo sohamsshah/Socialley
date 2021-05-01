aa16c167250810d48d741bc8e4be39a7cb894fbc
prerana1821 <prerananawar1@gmail.com>
2021-05-01 10:43:09 +0530

    feat: integrated tailwind css & added landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import { LandingPage } from "./pages/LandingPage/";
 
 function App() {
   return (
     <div className="App">
-      Hello World! I am Socialley.
+      {/* <h1 className="text-red-500">Hello World! I am Socialley.</h1> */}
+      <LandingPage />
     </div>
   );
 }
---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import LandingPageBanner from "./landing-page.jpg";
+// import { LandingPageBanner } from ".../images/landing-page.jpg";
+
+export const LandingPage = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <h1 className="text-center text-5xl m-4">Socialley</h1>
+      <div className="flex-grow">
+        <div className="my-9">
+          <img
+            className="object-contain md:object-scale-down"
+            src={LandingPageBanner}
+            alt="banner"
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <h2 className="text-center my-8 text-5xl font-bold md:font-bold">
+            A Valley for Socials
+          </h2>
+          <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded text-2xl">
+            Sign up
+          </button>
+        </div>
+      </div>
+      <footer className="flex flex-col items-center">
+        <p className="text-xl">© | 2021 | socially </p>
+        <p className="md:block cursor-pointer text-2xl hover:text-whit">
+          Socialley by neogGrammers
+        </p>
+        <p className="flex gap-6 my-2">
+          <i className="fab fa-2x fa-github"></i>
+          <i className="fab fa-2x fa-twitter"></i>
+        </p>
+      </footer>
+    </div>
+  );
+};
