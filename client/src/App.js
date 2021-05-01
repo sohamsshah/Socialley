6a5207e1a4f1603c19973e860a30da1bc54f2923
sohamsshah <sohamshah456@gmail.com>
2021-05-01 11:04:27 +0530

    complete testing express + socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import io from 'socket.io-client';
 
 function App() {
+  const socket = io.connect('http://localhost:8080', {transports: ['websocket']});
   return (
     <div className="App">
       Hello World! I am Socialley.
