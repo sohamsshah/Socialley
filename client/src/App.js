61364ddaee1747d1664a68238e3ee89a8d3baf42
sohamsshah <sohamshah456@gmail.com>
2021-05-01 11:59:19 +0530

    complete socket.io setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import io from 'socket.io-client';
+
 
 function App() {
+  const socket = io.connect('http://localhost:8080', {transports: ['websocket']});
   return (
     <div className="App">
       Hello World! I am Socialley.
--- server.js
+++ server.js
@@ -2,13 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
 // Data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req,res) => {
+    res.send({success:true});
+})
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
+io.on('connection', (socket)=> {
+    console.log("connected");
+});
 
-app.listen(PORT, console.log(`server started on port: ${PORT}`));
+
+const server_port = process.env.YOUR_PORT || process.env.PORT || PORT;
+server.listen(server_port, () => {
+    console.log("Server running on PORT : "+ server_port);
+})
