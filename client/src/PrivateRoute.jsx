43dc9eefc6fd4ea7f42af130809459c0f2224cee
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-05-01 15:15:06 +0530

    auth check

--- client/src/PrivateRoute.jsx
+++ client/src/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Route, Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export function PrivateRoute({ path, ...state }) {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
 
   return isAuthenticated ? (
     <Route {...state} path={path} />
