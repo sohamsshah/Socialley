f7f5d0b8b11b6e47c25bf4be49c6697618abd063
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-05-03 03:02:27 +0530

    remaned server to index

--- client/src/reducer/userReducer.js
+++ client/src/reducer/userReducer.js
@@ -1,7 +1,6 @@
 export const userReducer = (state, action) => {
   switch (action.type) {
     case "ADD_USER":
-      console.log(action.payload);
       return action.payload;
     default:
       console.log("Something wrong happened");
